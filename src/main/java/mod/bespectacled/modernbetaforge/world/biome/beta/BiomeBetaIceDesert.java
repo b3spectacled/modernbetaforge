8a9ec3cfe4714899c5d593652b339896cd2ead76
b3spectacled <modernbetamc@gmail.com>
2023-01-10 14:06:08 -0800

    Fix Ice Desert spawns.

--- src/main/java/mod/bespectacled/modernbetaforge/world/biome/beta/BiomeBetaIceDesert.java
+++ src/main/java/mod/bespectacled/modernbetaforge/world/biome/beta/BiomeBetaIceDesert.java
@@ -21,9 +21,10 @@ public class BiomeBetaIceDesert extends BiomeBeta {
         this.topBlock = BlockStates.SAND;
         this.fillerBlock = BlockStates.SAND;
         
+        // Should always clear to prevent passive mobs from spawning in desert
+        this.spawnableCreatureList.clear();
+        
         if (ModernBetaConfig.mobOptions.useNewMobs) {
-            this.spawnableCreatureList.clear();
-            
             this.spawnableCreatureList.add(new SpawnListEntry(EntityRabbit.class, 10, 2, 3));
             this.spawnableCreatureList.add(new SpawnListEntry(EntityPolarBear.class, 1, 1, 2));
             
