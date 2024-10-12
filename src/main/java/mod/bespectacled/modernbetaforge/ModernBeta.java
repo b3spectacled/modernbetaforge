251eb33ba191c4aeb7fad1e4c5e1af961deaebf6
b3spectacled <modernbetamc@gmail.com>
2024-10-11 21:03:55 -0700

    Fix AT not being applied in compiled mod.

--- src/main/java/mod/bespectacled/modernbetaforge/ModernBeta.java
+++ src/main/java/mod/bespectacled/modernbetaforge/ModernBeta.java
@@ -30,7 +30,7 @@ import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 public class ModernBeta {
     public static final String MODID = "modernbetaforge";
     public static final String NAME = "Modern Beta Forge";
-    public static final String VERSION = "1.0.5.0";
+    public static final String VERSION = "1.0.6.0";
     public static final String MCVERSION = "1.12.2";
 
     private static Logger logger = LogManager.getLogger(MODID);
