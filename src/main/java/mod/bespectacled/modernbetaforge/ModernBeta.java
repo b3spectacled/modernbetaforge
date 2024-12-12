8b85ae95fe45f4bf0d335f76d56134c91d83abc8
b3spectacled <modernbetamc@gmail.com>
2024-12-12 12:16:37 -0800

    Bump version.

--- src/main/java/mod/bespectacled/modernbetaforge/ModernBeta.java
+++ src/main/java/mod/bespectacled/modernbetaforge/ModernBeta.java
@@ -26,7 +26,7 @@ import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 public class ModernBeta {
     public static final String MODID = "modernbetaforge";
     public static final String NAME = "Modern Beta Forge";
-    public static final String VERSION = "1.3.2.0";
+    public static final String VERSION = "1.3.3.0";
     public static final String MCVERSION = "1.12.2";
     public static final int DATA_VERSION = 1310;
 
