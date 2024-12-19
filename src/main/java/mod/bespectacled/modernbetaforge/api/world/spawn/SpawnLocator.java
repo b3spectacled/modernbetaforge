8c1fe9a96863206a00196c0b5fa2259cac0fd312
b3spectacled <modernbetamc@gmail.com>
2024-12-19 03:06:58 -0800

    Fix default spawn locator spawning players below sea level on non-Skylands worlds.

--- src/main/java/mod/bespectacled/modernbetaforge/api/world/spawn/SpawnLocator.java
+++ src/main/java/mod/bespectacled/modernbetaforge/api/world/spawn/SpawnLocator.java
@@ -40,7 +40,7 @@ public interface SpawnLocator {
                         if (distance < r2) {
                             int y = chunkSource.getHeight(dX, dZ, HeightmapChunk.Type.SURFACE);
                             
-                            if (y > 32) {
+                            if (y > chunkSource.getSeaLevel()) {
                                 // Check if there are surrounding blocks, relevant for skylands worlds
                                 int numAdjacent = 0;
                                 for (int aX = dX - 1; aX <= dX + 1; ++aX) {
