668f36d5d6d0c7c087a7c89467dd286e38a8311a
b3spectacled <modernbetamc@gmail.com>
2025-01-29 13:36:44 -0800

    Simplify Infdev 611 chunk source.

--- src/main/java/mod/bespectacled/modernbetaforge/world/chunk/source/Infdev611ChunkSource.java
+++ src/main/java/mod/bespectacled/modernbetaforge/world/chunk/source/Infdev611ChunkSource.java
@@ -1,34 +1,9 @@
 package mod.bespectacled.modernbetaforge.world.chunk.source;
 
-import java.util.Random;
-
-import mod.bespectacled.modernbetaforge.util.noise.PerlinOctaveNoise;
 import mod.bespectacled.modernbetaforge.world.setting.ModernBetaGeneratorSettings;
 
 public class Infdev611ChunkSource extends AlphaChunkSource {
-    private final PerlinOctaveNoise beachOctaveNoise;
-    private final PerlinOctaveNoise surfaceOctaveNoise;
-    private final PerlinOctaveNoise forestOctaveNoise;
-    
     public Infdev611ChunkSource(long seed, ModernBetaGeneratorSettings settings) {
         super(seed, settings, true);
-
-        // Invoke noise constructors to seed surface and forest noise generators correctly,
-        // and also spin up new instance of Random since class member has been used in super constructor.
-        
-        Random random = this.createRandom(seed);
-        
-        new PerlinOctaveNoise(random, 16, true);
-        new PerlinOctaveNoise(random, 16, true);
-        new PerlinOctaveNoise(random, 8, true);
-        this.beachOctaveNoise = new PerlinOctaveNoise(random, 4, true);
-        this.surfaceOctaveNoise = new PerlinOctaveNoise(random, 4, true);
-        new PerlinOctaveNoise(random, 10, true);
-        new PerlinOctaveNoise(random, 16, true);
-        this.forestOctaveNoise = new PerlinOctaveNoise(random, 8, true);
-
-        this.setBeachOctaveNoise(this.beachOctaveNoise);
-        this.setSurfaceOctaveNoise(this.surfaceOctaveNoise);
-        this.setForestOctaveNoise(this.forestOctaveNoise);
     }
 }
