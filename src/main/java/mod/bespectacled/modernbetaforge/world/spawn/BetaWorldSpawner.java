1adabb3843995cbba6e14ce9595be60e35243763
b3spectacled <modernbetamc@gmail.com>
2025-03-06 00:18:50 -0800

    Fix Beta/PE spawn regression that broke gargamel spawn.

--- src/main/java/mod/bespectacled/modernbetaforge/world/spawn/BetaWorldSpawner.java
+++ src/main/java/mod/bespectacled/modernbetaforge/world/spawn/BetaWorldSpawner.java
@@ -10,10 +10,13 @@ import mod.bespectacled.modernbetaforge.api.world.chunk.surface.SurfaceBuilder;
 import mod.bespectacled.modernbetaforge.api.world.spawn.WorldSpawner;
 import mod.bespectacled.modernbetaforge.util.chunk.HeightmapChunk;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockPos.MutableBlockPos;
 import net.minecraftforge.common.BiomeDictionary;
 import net.minecraftforge.common.BiomeDictionary.Type;
 
 public class BetaWorldSpawner implements WorldSpawner {
+    private static final int MAX_ATTEMPTS = 10000;
+    
     @Override
     public BlockPos locateSpawn(BlockPos spawnPos, ChunkSource chunkSource, BiomeSource biomeSource) {
         SurfaceBuilder surfaceBuilder = ModernBetaRegistries.SURFACE_BUILDER
@@ -24,15 +27,17 @@ public class BetaWorldSpawner implements WorldSpawner {
         int z = 0;
         int attempts = 0;
         
+        MutableBlockPos mutablePos = new MutableBlockPos(x, 0, z);
         Random random = new Random();
         
         while(!this.isSandAt(x, z, chunkSource, biomeSource, surfaceBuilder, random)) {
-            if (attempts > 10000) {
+            if (attempts > MAX_ATTEMPTS) {
                 return WorldSpawner.DEFAULT.locateSpawn(spawnPos, chunkSource, biomeSource);
             }
-            
-            x += random.nextInt(64) - random.nextInt(64);
-            z += random.nextInt(64) - random.nextInt(64);
+
+            this.setRandomPosition(mutablePos, random);
+            x = mutablePos.getX();
+            z = mutablePos.getZ();
             
             attempts++;
         }
@@ -42,19 +47,33 @@ public class BetaWorldSpawner implements WorldSpawner {
         return new BlockPos(x, y, z);
     }
     
+    protected void setRandomPosition(MutableBlockPos mutablePos, Random random) {
+        int x = mutablePos.getX();
+        int z = mutablePos.getZ();
+        
+        x += random.nextInt(64) - random.nextInt(64);
+        z += random.nextInt(64) - random.nextInt(64);
+        
+        mutablePos.setPos(x, mutablePos.getY(), z);
+    }
+    
     private boolean isSandAt(int x, int z, ChunkSource chunkSource, BiomeSource biomeSource, SurfaceBuilder surfaceBuilder, Random random) {
         int y = chunkSource.getHeight(x, z, HeightmapChunk.Type.FLOOR);
         int seaLevel = chunkSource.getSeaLevel();
         
-        boolean isSandyBiome = BiomeDictionary.getBiomes(Type.SANDY).contains(biomeSource.getBiome(x, z)) && y >= seaLevel - 1;
+        boolean isSandyBiome = BiomeDictionary.getBiomes(Type.SANDY).contains(biomeSource.getBiome(x, z)) && this.aboveSeaLevel(y, seaLevel);
         
         if (surfaceBuilder instanceof NoiseSurfaceBuilder) {
             NoiseSurfaceBuilder noiseSurfaceBuilder = (NoiseSurfaceBuilder)surfaceBuilder;
-            boolean atBeachDepth = noiseSurfaceBuilder.atBeachDepth(y) && y >= seaLevel - 1;
+            boolean atBeachDepth = noiseSurfaceBuilder.atBeachDepth(y + 1) && this.aboveSeaLevel(y, seaLevel);
             
             return isSandyBiome || atBeachDepth && noiseSurfaceBuilder.isBeach(x, z, random) && !noiseSurfaceBuilder.isGravelBeach(x, z, random);
         }
         
         return isSandyBiome;
+    }
+    
+    private boolean aboveSeaLevel(int y, int seaLevel) {
+        return y >= seaLevel - 1;
     }
 }
--- src/main/java/mod/bespectacled/modernbetaforge/world/spawn/PEWorldSpawner.java
+++ src/main/java/mod/bespectacled/modernbetaforge/world/spawn/PEWorldSpawner.java
@@ -2,66 +2,24 @@ package mod.bespectacled.modernbetaforge.world.spawn;
 
 import java.util.Random;
 
-import mod.bespectacled.modernbetaforge.api.registry.ModernBetaRegistries;
-import mod.bespectacled.modernbetaforge.api.world.biome.source.BiomeSource;
-import mod.bespectacled.modernbetaforge.api.world.chunk.source.ChunkSource;
-import mod.bespectacled.modernbetaforge.api.world.chunk.surface.NoiseSurfaceBuilder;
-import mod.bespectacled.modernbetaforge.api.world.chunk.surface.SurfaceBuilder;
-import mod.bespectacled.modernbetaforge.api.world.spawn.WorldSpawner;
-import mod.bespectacled.modernbetaforge.util.chunk.HeightmapChunk;
-import net.minecraft.util.math.BlockPos;
-import net.minecraftforge.common.BiomeDictionary;
-import net.minecraftforge.common.BiomeDictionary.Type;
+import net.minecraft.util.math.BlockPos.MutableBlockPos;
 
-public class PEWorldSpawner implements WorldSpawner {
+public class PEWorldSpawner extends BetaWorldSpawner {
     @Override
-    public BlockPos locateSpawn(BlockPos spawnPos, ChunkSource chunkSource, BiomeSource biomeSource) {
-        SurfaceBuilder surfaceBuilder = ModernBetaRegistries.SURFACE_BUILDER
-            .get(chunkSource.getGeneratorSettings().surfaceBuilder)
-            .apply(chunkSource, chunkSource.getGeneratorSettings());
-
-        int x = 0;
-        int z = 0;
-        int attempts = 0;
+    protected void setRandomPosition(MutableBlockPos mutablePos, Random random) {
+        int x = mutablePos.getX();
+        int z = mutablePos.getZ();
         
-        Random random = new Random();
+        x += random.nextInt(32) - random.nextInt(32);
+        z += random.nextInt(32) - random.nextInt(32);
         
-        while(!this.isSandAt(x, z, chunkSource, biomeSource, surfaceBuilder, random)) {
-            if (attempts > 10000) {
-                return WorldSpawner.DEFAULT.locateSpawn(spawnPos, chunkSource, biomeSource);
-            }
-            
-            x += random.nextInt(32) - random.nextInt(32);
-            z += random.nextInt(32) - random.nextInt(32);
-            
-            // Keep spawn pos within bounds of original PE world size
-            if (x < 4) x += 32;
-            if (x >= 251) x -= 32;
-            
-            if (z < 4) z += 32;
-            if (z >= 251) z -= 32;
-            
-            attempts++;
-        }
+        // Keep spawn pos within bounds of original PE world size
+        if (x < 4) x += 32;
+        if (x >= 251) x -= 32;
         
-        int y = chunkSource.getHeight(x, z, HeightmapChunk.Type.FLOOR) + 1;
+        if (z < 4) z += 32;
+        if (z >= 251) z -= 32;
         
-        return new BlockPos(x, y, z);
-    }
-    
-    private boolean isSandAt(int x, int z, ChunkSource chunkSource, BiomeSource biomeSource, SurfaceBuilder surfaceBuilder, Random random) {
-        int y = chunkSource.getHeight(x, z, HeightmapChunk.Type.FLOOR);
-        int seaLevel = chunkSource.getSeaLevel();
-        
-        boolean isSandyBiome = BiomeDictionary.getBiomes(Type.SANDY).contains(biomeSource.getBiome(x, z)) && y >= seaLevel - 1;
-        
-        if (surfaceBuilder instanceof NoiseSurfaceBuilder) {
-            NoiseSurfaceBuilder noiseSurfaceBuilder = (NoiseSurfaceBuilder)surfaceBuilder;
-            boolean atBeachDepth = noiseSurfaceBuilder.atBeachDepth(y) && y >= seaLevel - 1;
-            
-            return isSandyBiome || atBeachDepth && noiseSurfaceBuilder.isBeach(x, z, random) && !noiseSurfaceBuilder.isGravelBeach(x, z, random);
-        }
-        
-        return isSandyBiome;
+        mutablePos.setPos(x, mutablePos.getY(), z);
     }
 }
