c8e8869d94ea23045b8cfb5f49afd15df5a79558
b3spectacled <modernbetamc@gmail.com>
2025-08-13 18:21:29 -0700

    Add documentation for client registries.

--- src/main/java/mod/bespectacled/modernbetaforge/api/registry/ModernBetaClientRegistries.java
+++ src/main/java/mod/bespectacled/modernbetaforge/api/registry/ModernBetaClientRegistries.java
@@ -2,12 +2,22 @@ package mod.bespectacled.modernbetaforge.api.registry;
 
 import mod.bespectacled.modernbetaforge.api.client.gui.GuiCustomizePreset;
 import mod.bespectacled.modernbetaforge.api.client.gui.GuiPredicate;
+import mod.bespectacled.modernbetaforge.world.setting.ModernBetaGeneratorSettings;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 @SideOnly(Side.CLIENT)
 public class ModernBetaClientRegistries {
+    /**
+     * Holds registered {@link GuiCustomizePreset world presets}.
+     * Register world presets here.
+     */
     public static final ModernBetaRegistry<GuiCustomizePreset> GUI_PRESET;
+    
+    /**
+     * Holds registered {@link GuiPredicate GUI predicates}.
+     * Register predicates to enable or disable GUI buttons based on the state of {@link ModernBetaGeneratorSettings} in the world customization GUI.
+     */
     public static final ModernBetaRegistry<GuiPredicate> GUI_PREDICATE;
     
     static {
