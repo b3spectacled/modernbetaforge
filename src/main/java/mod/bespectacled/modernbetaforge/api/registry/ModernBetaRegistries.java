2dbd0b6bf2ceb07f9597095cc1c88eca354503d8
b3spectacled <modernbetamc@gmail.com>
2025-09-04 00:19:12 -0700

    Remove unnecessary TODO.

--- src/main/java/mod/bespectacled/modernbetaforge/api/registry/ModernBetaRegistries.java
+++ src/main/java/mod/bespectacled/modernbetaforge/api/registry/ModernBetaRegistries.java
@@ -158,7 +158,6 @@ public class ModernBetaRegistries {
     
     @FunctionalInterface
     public static interface BiomeResolverCreator {
-        // TODO 1.9.0.0: Update interface to include BiomeSource.
         BiomeResolverCustom apply(ChunkSource chunkSource, ModernBetaGeneratorSettings settings);
     }
     
