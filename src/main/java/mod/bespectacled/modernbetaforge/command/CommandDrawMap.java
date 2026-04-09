050aaf56bfe2e15b90ce65f130064685c4cba189
b3spectacled <modernbetamc@gmail.com>
2026-04-09 14:27:35 -0700

    Fix server crash due to drawmap command.

--- src/main/java/mod/bespectacled/modernbetaforge/command/CommandDrawMap.java
+++ src/main/java/mod/bespectacled/modernbetaforge/command/CommandDrawMap.java
@@ -36,9 +36,9 @@ import net.minecraft.world.biome.BiomeProvider;
 import net.minecraft.world.gen.IChunkGenerator;
 
 public class CommandDrawMap extends ModernBetaCommand {
-    private static final File SCREENSHOT_DIR = new File(Minecraft.getMinecraft().gameDir, "screenshots");
     private static final DateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd_HH.mm.ss");
     private static final String NAME = "drawmap";
+    private static final int MAX_SIZE = 5120;
     
     private int percentage = 0;
     
@@ -46,6 +46,51 @@ public class CommandDrawMap extends ModernBetaCommand {
         super(NAME);
     }
     
+    @Override
+    public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
+        WorldServer worldServer = this.validateWorld(server, sender);
+        this.validateArgsLength(sender, args, 2, 3);
+        
+        if (!server.isSinglePlayer()) {
+            throw new CommandException(this.getLangString("sp"), new Object[0]);
+        }
+        
+        boolean success = false;
+        
+        // Reverse for natural dimensions
+        int length = MathHelper.clamp(CommandBase.parseInt(args[0]) >> 4 << 4, 0, MAX_SIZE);
+        int width = MathHelper.clamp(CommandBase.parseInt(args[1]) >> 4 << 4, 0, MAX_SIZE);
+        
+        BlockPos center = args.length == 2 || !CommandBase.parseBoolean(args[2]) ? sender.getPosition() : BlockPos.ORIGIN;
+        center = new BlockPos(center.getX() >> 4 << 4, center.getY(), center.getZ() >> 4 << 4);
+    
+        try {
+            File dir = new File(Minecraft.getMinecraft().gameDir, "screenshots");
+            dir.mkdir();
+            
+            File file = getTimestampedPNGFileForDirectory(dir);
+            file = file.getCanonicalFile(); // Fixes '/./' being inserted in path
+    
+            notifyCommandListener(sender, this, this.getLangString("start"), new Object[] { width, length, center.getX(), center.getZ() });
+            BufferedImage image = this.drawMap(worldServer, center, length, width, current -> setProgress(current, sender));
+            ImageIO.write(image, "png", file);
+            
+            ITextComponent textComponent = new TextComponentString(file.getName());
+            textComponent.getStyle().setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_FILE, file.getPath()));
+            textComponent.getStyle().setUnderlined(Boolean.valueOf(true));
+            
+            notifyCommandListener(sender, this, this.getLangString("success"), new Object[] { textComponent });
+            success = true;
+            
+        } catch (Exception e) {
+            ModernBeta.log(Level.WARN, String.format("Command '%s' failed!", this.name));
+        }
+    
+        if (!success) {
+            throw new CommandException(this.getLangString("failure"), new Object[0]);
+        }
+    }
+
     public BufferedImage drawMap(WorldServer worldServer, BlockPos center, int width, int length, Consumer<Float> progressTracker) throws IllegalStateException {
         IChunkGenerator chunkGenerator = worldServer.getChunkProvider().chunkGenerator;
         BiomeProvider biomeProvider = worldServer.getBiomeProvider();
@@ -81,44 +126,6 @@ public class CommandDrawMap extends ModernBetaCommand {
         throw new IllegalStateException();
     }
 
-    @Override
-    public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
-        WorldServer worldServer = this.validateWorld(server, sender);
-        this.validateArgsLength(sender, args, 2, 3);
-        
-        boolean success = false;
-        
-        // Reverse for natural dimensions
-        int length = MathHelper.clamp(CommandBase.parseInt(args[0]) >> 4 << 4, 0, 5120);
-        int width = MathHelper.clamp(CommandBase.parseInt(args[1]) >> 4 << 4, 0, 5120);
-        
-        BlockPos center = args.length == 2 || !CommandBase.parseBoolean(args[2]) ? sender.getPosition() : BlockPos.ORIGIN;
-        center = new BlockPos(center.getX() >> 4 << 4, center.getY(), center.getZ() >> 4 << 4);
-
-        try { 
-            File file = getTimestampedPNGFileForDirectory(SCREENSHOT_DIR);
-            file = file.getCanonicalFile(); // Fixes '/./' being inserted in path
-
-            notifyCommandListener(sender, this, this.getLangString("start"), new Object[] { width, length, center.getX(), center.getZ() });
-            BufferedImage image = this.drawMap(worldServer, center, length, width, current -> setProgress(current, sender));
-            ImageIO.write(image, "png", file);
-            
-            ITextComponent textComponent = new TextComponentString(file.getName());
-            textComponent.getStyle().setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_FILE, file.getPath()));
-            textComponent.getStyle().setUnderlined(Boolean.valueOf(true));
-            
-            notifyCommandListener(sender, this, this.getLangString("success"), new Object[] { textComponent });
-            success = true;
-            
-        } catch (Exception e) {
-            ModernBeta.log(Level.WARN, String.format("Command '%s' failed!", this.name));
-        }
-
-        if (!success) {
-            throw new CommandException(this.getLangString("failure"), new Object[0]);
-        }
-    }
-    
     private void setProgress(float current, ICommandSender sender) {
         int percentage = (int)(current * 100.0f);
         
