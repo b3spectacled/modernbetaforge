23802e2e733955bf34c1c51fb658ea987b0aa748
b3spectacled <modernbetamc@gmail.com>
2026-08-10 01:51:16 -0700

    Add additional ravine exception biomes. Optimize ravine blockpos usage.

--- src/main/java/mod/bespectacled/modernbetaforge/world/carver/MapGenRavineExtended.java
+++ src/main/java/mod/bespectacled/modernbetaforge/world/carver/MapGenRavineExtended.java
@@ -5,11 +5,12 @@ import java.util.Set;
 import com.google.common.collect.ImmutableSet;
 
 import mod.bespectacled.modernbetaforge.api.world.chunk.source.ChunkSource;
+import mod.bespectacled.modernbetaforge.world.biome.ModernBetaBiomeHolders;
 import mod.bespectacled.modernbetaforge.world.setting.ModernBetaGeneratorSettings;
 import net.minecraft.block.Block;
 import net.minecraft.init.Biomes;
 import net.minecraft.init.Blocks;
-import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockPos.MutableBlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.chunk.ChunkPrimer;
@@ -21,11 +22,13 @@ public class MapGenRavineExtended extends MapGenRavine {
     private final Block defaultBlock;
     private final Set<Block> defaultFluids;
     private final int ravineChance;
+    private final MutableBlockPos mutablePos;
 
     public MapGenRavineExtended(ChunkSource chunkSource, ModernBetaGeneratorSettings settings) {
         this.defaultBlock = chunkSource.getDefaultBlock().getBlock();
         this.defaultFluids = MapGenBetaCave.getDefaultFluids(chunkSource.getDefaultFluid());
         this.ravineChance = settings.ravineChance;
+        this.mutablePos = new MutableBlockPos();
     }
     
     @Override
@@ -48,7 +51,7 @@ public class MapGenRavineExtended extends MapGenRavine {
     
     @Override
     protected void digBlock(ChunkPrimer chunkPrimer, int x, int y, int z, int chunkX, int chunkZ, boolean foundTop) {
-        Biome biome = this.world.getBiome(new BlockPos(x + chunkX * 16, 0, z + chunkZ * 16));
+        Biome biome = this.world.getBiome(this.mutablePos.setPos(x + chunkX * 16, 0, z + chunkZ * 16));
         
         Block block = chunkPrimer.getBlockState(x, y, z).getBlock();
         Block topBlock = Blocks.GRASS;
@@ -87,6 +90,10 @@ public class MapGenRavineExtended extends MapGenRavine {
             .add(Biomes.DESERT)
             .add(Biomes.MUSHROOM_ISLAND)
             .add(Biomes.MUSHROOM_ISLAND_SHORE)
+            .add(ModernBetaBiomeHolders.BETA_BEACH)
+            .add(ModernBetaBiomeHolders.BETA_SNOWY_BEACH)
+            .add(ModernBetaBiomeHolders.BETA_DESERT)
+            .add(ModernBetaBiomeHolders.BETA_ICE_DESERT)
             .build();
     }
 }
