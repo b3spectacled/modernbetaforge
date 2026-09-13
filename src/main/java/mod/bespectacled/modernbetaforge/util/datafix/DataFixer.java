7f46fbab71966f28ac93b5a8ef0ccf89b6296189
b3spectacled <modernbetamc@gmail.com>
2026-09-13 13:12:11 -0700

    Reduce datafixer log spam.

--- src/main/java/mod/bespectacled/modernbetaforge/util/datafix/DataFixer.java
+++ src/main/java/mod/bespectacled/modernbetaforge/util/datafix/DataFixer.java
@@ -10,11 +10,13 @@ import mod.bespectacled.modernbetaforge.api.datafix.DataFix;
 
 public class DataFixer {
     public static void runDataFixer(DataFix[] dataFixes, JsonObject jsonObject, String worldName, int fixVersion) {
+        logDataFixer(worldName, fixVersion);
+        
         for (int i = 0; i < dataFixes.length; ++i) {
             DataFix dataFix = dataFixes[i];
             String tag = dataFix.getTag();
             
-            logDataFix(tag, worldName, fixVersion);
+            // logDataFix(tag, worldName, fixVersion);
             
             JsonElement newValue = dataFixes[i].getDataFixer().apply(ModDataFixer.getAsJsonObject(jsonObject.toString()));
             if (newValue != null) {
@@ -23,6 +25,11 @@ public class DataFixer {
         }
     }
     
+    private static void logDataFixer(String worldName, int fixVersion) {
+        ModernBeta.log(Level.DEBUG, String.format("[DataFix] Found world '%s' to be fixed for version '%d'..", worldName, fixVersion));
+    }
+    
+    @SuppressWarnings("unused")
     private static void logDataFix(String key, String worldName, int fixVersion) {
         ModernBeta.log(Level.DEBUG, String.format("[DataFix] Found property '%s' in world '%s' to be fixed for version '%d'..", key, worldName, fixVersion));
     }
