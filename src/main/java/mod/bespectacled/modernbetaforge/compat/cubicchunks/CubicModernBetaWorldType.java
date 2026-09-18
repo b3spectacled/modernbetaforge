95646a03bfb6b26a24829b6f28b0b39895fef452
b3spectacled <modernbetamc@gmail.com>
2026-09-18 10:33:28 -0700

    More small adjustments.

--- src/main/java/mod/bespectacled/modernbetaforge/compat/cubicchunks/CubicModernBetaWorldType.java
+++ src/main/java/mod/bespectacled/modernbetaforge/compat/cubicchunks/CubicModernBetaWorldType.java
@@ -3,10 +3,10 @@ package mod.bespectacled.modernbetaforge.compat.cubicchunks;
 import io.github.opencubicchunks.cubicchunks.api.util.IntRange;
 import io.github.opencubicchunks.cubicchunks.api.world.ICubicWorldType;
 import io.github.opencubicchunks.cubicchunks.api.worldgen.ICubeGenerator;
-import io.github.opencubicchunks.cubicchunks.core.world.provider.ICubicWorldProvider;
 import mod.bespectacled.modernbetaforge.world.AdjustableCloudHeightWorldType;
 import mod.bespectacled.modernbetaforge.world.ModernBetaWorldType;
 import mod.bespectacled.modernbetaforge.world.chunk.ModernBetaChunkGenerator;
+import mod.bespectacled.modernbetaforge.world.setting.ModernBetaGeneratorSettings;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldServer;
 import net.minecraftforge.common.DimensionManager;
@@ -18,7 +18,8 @@ public class CubicModernBetaWorldType extends ModernBetaWorldType implements ICu
 
     @Override
     public IntRange calculateGenerationHeightRange(WorldServer worldServer) {
-        return new IntRange(0, ((ICubicWorldProvider)worldServer.provider).getOriginalActualHeight());
+        ModernBetaGeneratorSettings settings = ModernBetaGeneratorSettings.buildOrGet(worldServer);
+        return new IntRange(settings.floor, settings.height);
     }
 
     @Override
